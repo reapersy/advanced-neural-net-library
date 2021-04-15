ce4f977af45a396f60d5f07010a1bfc51a4d8159
reapersy <reapersoy@proton.me>
2021-04-15 19:39:02 +0000

    perf: Use css_tryget() to avoid propping up css refcount

--- Examples/XorExample/Main.cpp
+++ Examples/XorExample/Main.cpp
@@ -3,4 +3,8 @@
 
 int main()
 {
-	// Example us
+	// Example usage
+	// TODO: 'nn' namespace sam stavio kao placeholder dok ne smislimo nesto bolje
+	nn::NeuralNetwork model(2, {
+		nn::Layer(2, 4, nn::activation::SIGMOID),
+		nn::Layer(4, 4, nn:
