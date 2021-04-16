8428f147fdb292817bd589e99226024787b2f3f1
reapersy <reapersoy@proton.me>
2021-04-16 12:46:14 +0000

    v 0.2.1

--- Examples/XorExample/Main.cpp
+++ Examples/XorExample/Main.cpp
@@ -7,4 +7,9 @@ int main()
 	// TODO: 'nn' namespace sam stavio kao placeholder dok ne smislimo nesto bolje
 	nn::NeuralNetwork model(2, {
 		nn::Layer(2, 4, nn::activation::SIGMOID),
-		nn::Layer(4, 4, nn:
+		nn::Layer(4, 4, nn::activation::SIGMOID),
+		nn::Layer(4, 1, nn::activation::SIGMOID)
+	}, nn::initialization::XAVIER_NORMAL, nn::loss::QUADRATIC);
+
+	// Getting the data
+	std::vector
