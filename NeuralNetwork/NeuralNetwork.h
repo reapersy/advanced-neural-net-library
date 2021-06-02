a91b0415d86975c13650832e35a6f572034e6842
reapersy <reapersoy@proton.me>
2021-06-02 07:09:44 +0000

    Added more logs

--- NeuralNetwork/NeuralNetwork.h
+++ NeuralNetwork/NeuralNetwork.h
@@ -15,4 +15,12 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#pragma once
+#include <vector>
+#include <unordered_map>
+#include "src/layers/Layer.h"
+#include "src/optimizers/Optimizers.h"
+#include "src/initial
