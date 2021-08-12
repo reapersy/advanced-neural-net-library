522c028941a3ccfe9782bccf33680f78f57293a0
reapersy <reapersoy@proton.me>
2021-08-12 18:24:27 +0000

    modify dockerfile

--- NeuralNetwork/src/activations/ELU.cpp
+++ NeuralNetwork/src/activations/ELU.cpp
@@ -30,4 +30,13 @@ namespace nn
 
 		Matrix ELu::Function(Matrix& x)
 		{
-			return x.Map([alph=alpha](double a) { return a >= 0 ? a : alph*(exp(a) - 1)
+			return x.Map([alph=alpha](double a) { return a >= 0 ? a : alph*(exp(a) - 1); });
+		}
+
+		Matrix ELu::Derivative(Matrix& x)
+		{
+			return x.Map([alph=alpha](double a) { return a >= 0 ? 1 : alph*exp(a); });
+		}
+
+		Type ELu::GetType() const
+		
