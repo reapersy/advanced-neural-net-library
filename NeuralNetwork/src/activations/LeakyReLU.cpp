6082f3a2c16e6ee19ddea751eaad95ff1a58ed3b
reapersy <reapersoy@proton.me>
2021-09-01 06:32:43 +0000

    HID: ntrig: validate feature report details

--- NeuralNetwork/src/activations/LeakyReLU.cpp
+++ NeuralNetwork/src/activations/LeakyReLU.cpp
@@ -15,4 +15,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "ActivationFunctions.h"
+
+namespace nn
+{
+	namespace activation
+	{
+		LeakyReLu::LeakyReLu(double alpha) : alpha(a
