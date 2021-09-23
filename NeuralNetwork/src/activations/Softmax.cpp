a33d1315ae33d2da2be1961a50f8054f948dcf97
reapersy <reapersoy@proton.me>
2021-09-23 08:15:05 +0000

    Unreviewed. Rebaselined run-bindings-tests results.

--- NeuralNetwork/src/activations/Softmax.cpp
+++ NeuralNetwork/src/activations/Softmax.cpp
@@ -15,4 +15,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-a
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "ActivationFunctions.h"
+
+namespace nn
+{
+	namespace activation
+	{
+		Matrix Softmax::Functio
