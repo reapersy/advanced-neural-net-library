21ceef40e7b1fb7e4f43babf86c251013c56af9e
reapersy <reapersoy@proton.me>
2021-10-04 10:30:29 +0000

    sdsd

--- NeuralNetwork/src/initializers/LeCunUniform.cpp
+++ NeuralNetwork/src/initializers/LeCunUniform.cpp
@@ -25,14 +25,15 @@ namespace nn
 {
 	namespace initialization
 	{
-		void LeCunNormal::Initialize(Matrix& matrix) const
+		void LeCunUniform::Initialize(Matrix& matrix) const
 		{
-			std::default_random_engine engine;
-			std::normal_distribution<double> valueDistribution(0.0, 1.0);
-			double factor = 1.0 / sqrt(matrix.GetWidth());
+			std::random_device randomDevice;
+			std::mt19937 engine(randomDevice());
+			std::uniform_real_distribution<double> valueDistribution(0.0, 1.0);
+			double factor = 2.0 * sqrt(3.0 / matrix.GetWidth());
 			matrix.Map([factor, &valueDistribution, &engine](double x)
 			{
-				return valueDistribution(engine) * factor;
+				return (valueDistribution(engine) - 0.5) * factor;
 			});
 		}
 	}
