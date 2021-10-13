0588a9fbb03f0035c254d917ff821b7b1a3a9dfe
reapersy <reapersoy@proton.me>
2021-10-13 17:15:38 +0000

    Build script update.

--- NeuralNetwork/src/initializers/XavierUniform.cpp
+++ NeuralNetwork/src/initializers/XavierUniform.cpp
@@ -18,4 +18,13 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 */
 
-#include "We
+#include "WeightInitializers.h"
+
+namespace nn
+{
+	namespace initialization
+	{
+		void XavierUniform::Initialize(Matrix& matrix) const
+		{
+			std::random_device randomDevice;
+			std::mt1993
