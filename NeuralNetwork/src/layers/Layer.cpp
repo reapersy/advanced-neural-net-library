18bc66a83f50487e0dde30e3153c51340e5140c9
reapersy <reapersoy@proton.me>
2021-10-16 09:17:46 +0000

    Run scan-local.sh on swipl:...

--- NeuralNetwork/src/layers/Layer.cpp
+++ NeuralNetwork/src/layers/Layer.cpp
@@ -44,3 +44,47 @@ namespace nn
 	Layer::Layer(Layer && layer) noexcept : WeightMatrix(std::move(layer.WeightMatrix)), BiasMatrix(std::move(layer.BiasMatrix)),
 		Activation(std::move(layer.Activation)), WeightedSum(std::move(layer.WeightedSum)), ActivationFunction(std::move(layer.ActivationFunction))
 	{
+
+	}
+
+	Layer::Layer(const Layer & layer) : WeightMatrix(layer.WeightMatrix), BiasMatrix(layer.BiasMatrix),
+		Activation(layer.Activation), WeightedSum(layer.WeightedSum), ActivationFunction(layer.ActivationFunction)
+	{
+	}
+
+	void Layer::Initialize(const std::shared_ptr<initialization::Initializer> initializer)
+	{
+		initializer->Initialize(WeightMatrix);
+	}
+
+	void Layer::SaveLayer(std::ofstream & outfile) const
+	{
+		WeightMatrix.SaveMatrix(outfile);
+		BiasMatrix.SaveMatrix(outfile);
+		ActivationFunction->SaveActivationFunction(outfile);
+	}
+
+	Layer Layer::LoadLayer(std::ifstream & infile)
+	{
+		Matrix weightMatrix = Matrix::LoadMatrix(infile);
+		Matrix biasMatrix = Matrix::LoadMatrix(infile);
+		int activationType;
+		infile.read((char*)&activationType, sizeof(activationType));
+		activation::Type type = activation::Type(activationType);
+		Layer layer(weightMatrix.GetWidth(), weightMatrix.GetHeight(), type);
+		layer.WeightMatrix = std::move(weightMatrix);
+		layer.BiasMatrix = std::move(biasMatrix);
+		return layer;
+	}
+	Layer & Layer::operator=(Layer && layer)
+	{
+		WeightMatrix = std::move(layer.WeightMatrix);
+		BiasMatrix = std::move(layer.BiasMatrix);
+		Activation = std::move(layer.Activation);
+		ActivationFunction = std::move(layer.ActivationFunction);
+		WeightedSum = std::move(layer.WeightedSum);
+		return *this;
+	}
+
+}
+
