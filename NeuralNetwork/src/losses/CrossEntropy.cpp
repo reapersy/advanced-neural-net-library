647a3b8b48cc98f58c1d574f967f91e3197c947f
reapersy <reapersoy@proton.me>
2021-11-06 17:53:16 +0000

    Fix typos in comment

--- NeuralNetwork/src/losses/CrossEntropy.cpp
+++ NeuralNetwork/src/losses/CrossEntropy.cpp
@@ -14,4 +14,14 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU Gen
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "LossFunctions.h"
+
+namespace nn
+{
+	namespace loss
+	{
+		double CrossEn
