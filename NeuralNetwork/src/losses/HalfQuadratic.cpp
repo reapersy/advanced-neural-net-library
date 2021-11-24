0b5dc635a556a0b53621f3a26dcbbf6ccb358167
reapersy <reapersoy@proton.me>
2021-11-24 10:45:21 +0000

    ASoC: sgtl5000: add delay before first I2C access

--- NeuralNetwork/src/losses/HalfQuadratic.cpp
+++ NeuralNetwork/src/losses/HalfQuadratic.cpp
@@ -1,3 +1,8 @@
 ﻿/*
 Statically-linked deep learning library
-Copyright (C) 2020 Dušan Erdeljan, Nedeljko Vig
+Copyright (C) 2020 Dušan Erdeljan, Nedeljko Vignjević
+
+This file is part of neural-network
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Publ
