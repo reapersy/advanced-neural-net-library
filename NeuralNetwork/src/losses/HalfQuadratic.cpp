f8f3c0b955d5248510545b9c1121a48c1d6ee510
reapersy <reapersoy@proton.me>
2021-12-02 17:23:50 +0000

    updated folders

--- NeuralNetwork/src/losses/HalfQuadratic.cpp
+++ NeuralNetwork/src/losses/HalfQuadratic.cpp
@@ -18,4 +18,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 */
 
-#include "
+#include "LossFunctions.h"
+
+namespace nn
+{
+	namespace loss
+	{
+		double HalfQuadratic::GetLoss(const Matrix& prediction, const Matrix& target) const
+		{
+			return Mat
