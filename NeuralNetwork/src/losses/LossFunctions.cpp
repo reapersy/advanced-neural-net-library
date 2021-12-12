4d7fb8773e8bd611626176ad4d330ea99238cbb9
reapersy <reapersoy@proton.me>
2021-12-12 22:03:19 +0000

    Sync with WeblateOrg/meta

--- NeuralNetwork/src/losses/LossFunctions.cpp
+++ NeuralNetwork/src/losses/LossFunctions.cpp
@@ -16,4 +16,14 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
-*
+*/
+
+#include "LossFunctions.h"
+
+namespace nn
+{
+	namespace loss
+	{
+		Matrix LossFunction::Backward(Layer & layer, Matrix & error)
+		{
+			Matrix gradient = layer.ActivationF
