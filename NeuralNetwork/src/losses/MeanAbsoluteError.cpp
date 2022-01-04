79260f61c9a5c05c5b08add65d57915c6f5ffc26
reapersy <reapersoy@proton.me>
2022-01-04 13:55:40 +0000

    random cleanup

--- NeuralNetwork/src/losses/MeanAbsoluteError.cpp
+++ NeuralNetwork/src/losses/MeanAbsoluteError.cpp
@@ -14,4 +14,13 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU 
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "LossFunctions.h"
+
+namespace nn
+{
+	namespace loss
+	{
