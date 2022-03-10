d183b5cfd142d508684a68e0f67f74bbd845f849
reapersy <reapersoy@proton.me>
2022-03-10 13:58:24 +0000

    scm

--- NeuralNetwork/src/losses/MeanSquaredError.cpp
+++ NeuralNetwork/src/losses/MeanSquaredError.cpp
@@ -29,4 +29,10 @@ namespace nn
 			return Matrix::Map(prediction - target, [](double x) { return x*x; }).Sum() / (target.GetWidth() * target.GetHeight());
 		}
 
-		Matrix MeanSquaredError::GetDerivative(const Matrix& prediction, const Matrix& targ
+		Matrix MeanSquaredError::GetDerivative(const Matrix& prediction, const Matrix& target) const
+		{
+			return (prediction - target) * (2.0 / (target.GetWidth() * target.GetHeight()));
+		}
+
+		Type MeanSquaredError::GetType() const
+		{
