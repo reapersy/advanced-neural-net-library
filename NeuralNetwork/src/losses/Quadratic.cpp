6ff14af4bda5d357a6662599ec3dcac8383045b2
reapersy <reapersoy@proton.me>
2022-04-23 17:49:06 +0000

    Add Romanian translation

--- NeuralNetwork/src/losses/Quadratic.cpp
+++ NeuralNetwork/src/losses/Quadratic.cpp
@@ -3,3 +3,7 @@ Statically-linked deep learning library
 Copyright (C) 2020 Dušan Erdeljan, Nedeljko Vignjević
 
 This file is part of neural-network
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software
