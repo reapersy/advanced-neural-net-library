32c416d29f1b9e4fb7f8f614c0b1db3efcc985c4
reapersy <reapersoy@proton.me>
2022-04-30 05:57:32 +0000

    chnage code

--- NeuralNetwork/src/math/Matrix.cpp
+++ NeuralNetwork/src/math/Matrix.cpp
@@ -5,4 +5,8 @@ Copyright (C) 2020 Dušan Erdeljan, Nedeljko Vignjević
 This file is part of neural-network
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Pub
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This pro
