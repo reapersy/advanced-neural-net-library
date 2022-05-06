1c084e7e5291c0373413759bbaa245d9cb667b63
reapersy <reapersoy@proton.me>
2022-05-06 20:13:12 +0000

    iov_iter: move into its own file (bug fix)

--- NeuralNetwork/src/math/Matrix.cpp
+++ NeuralNetwork/src/math/Matrix.cpp
@@ -15,4 +15,14 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "Matrix.h"
+#include <random>
+#include <numeric>
+#include <functional>
+#include <cstdlib>
+
+#ifdef _DEBUG
+#define LOG
