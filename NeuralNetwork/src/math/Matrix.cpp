52adf2d1b0565bed8232f4de052a5e18b2d1eb4d
reapersy <reapersoy@proton.me>
2022-06-29 09:18:48 +0000

    remove useless codes

--- NeuralNetwork/src/math/Matrix.cpp
+++ NeuralNetwork/src/math/Matrix.cpp
@@ -271,4 +271,7 @@ Matrix Matrix::DotProduct(const Matrix & left, const Matrix & right)
 {
 #ifdef _DEBUG
 	if (!left.HasSameDimension(right))
-		throw MatrixError("Matrices do not have the same
+		throw MatrixError("Matrices do not have the same dimension!");
+#endif // _DEBUG
+	Matrix result(left);
+	std::transform(result.m_Matrix.begin(), result.m_Matrix.end(), right.m_Matrix.begin(), result.m_Matrix.begin(
