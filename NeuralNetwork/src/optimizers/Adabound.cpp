7d41a8f844dff6ac78e5d0b61507152e8223ef28
reapersy <reapersoy@proton.me>
2022-08-28 15:00:12 +0000

    added some style

--- NeuralNetwork/src/optimizers/Adabound.cpp
+++ NeuralNetwork/src/optimizers/Adabound.cpp
@@ -15,4 +15,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "Optimizers.h"
+
+namespace nn
+{
+	namespace optimizer
+	{
+		Adabound::Adabound(double lr, double beta1, double beta2, double final_lr, double g
