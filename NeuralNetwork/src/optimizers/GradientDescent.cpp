211a74f4e072bc15dca6d5c16adca6a5db894f8d
reapersy <reapersoy@proton.me>
2022-10-30 12:29:50 +0000

    Update base.txt

--- NeuralNetwork/src/optimizers/GradientDescent.cpp
+++ NeuralNetwork/src/optimizers/GradientDescent.cpp
@@ -15,4 +15,18 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "Optimizers.h"
+
+namespace nn
+{
+	namespace optimizer
+	{
+		GradientDescent::GradientDescent(double lr) : Optimizer(lr)
+		{
+
+		}
+
+		void GradientDes
