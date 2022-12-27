32cd1ab0f2faba5b4332facea0c754ad0fe3ea37
reapersy <reapersoy@proton.me>
2022-12-27 13:45:39 +0000

    Add google analytics tracking code

--- NeuralNetwork/src/optimizers/RMSProp.cpp
+++ NeuralNetwork/src/optimizers/RMSProp.cpp
@@ -17,3 +17,39 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "Optimizers.h"
+
+namespace nn
+{
+	namespace optimizer
+	{
+		RMSProp::RMSProp(double lr, double beta) : Optimizer(lr), m_Beta(beta)
+		{
+
+		}
+
+		void RMSProp::UpdateLayer(Layer& layer, Matrix& deltaWeight, Matrix& deltaBias, int layerIndex, unsigned int epoch)
+		{
+			if (gradSquaredW.find(layerIndex) == gradSquaredW.end())
+			{
+				gradSquaredW[layerIndex] = (1 - m_Beta) * Matrix::Map(deltaWeight, [](double x) { return x*x; });
+				gradSquaredB[layerIndex] = (1 - m_Beta) * Matrix::Map(deltaBias, [](double x) { return x*x; });
+			}
+			else
+			{
+				gradSquaredW[layerIndex] = m_Beta * gradSquaredW[layerIndex] + (1 - m_Beta) * Matrix::Map(deltaWeight, [](double x) { return x*x; });
+				gradSquaredB[layerIndex] = m_Beta * gradSquaredB[layerIndex] + (1 - m_Beta) * Matrix::Map(deltaBias, [](double x) { return x*x; });
+			}
+			layer.WeightMatrix -= (m_LearningRate * deltaWeight) / Matrix::Map(gradSquaredW[layerIndex], [](double x) { return sqrt(x) + 1e-7; });
+			layer.BiasMatrix -= (m_LearningRate * deltaBias) / Matrix::Map(gradSquaredB[layerIndex], [](double x) { return sqrt(x) + 1e-7; });
+		}
+
+		void RMSProp::Reset()
+		{
+			gradSquaredB.clear();
+			gradSquaredW.clear();
+		}
+	}
+}
