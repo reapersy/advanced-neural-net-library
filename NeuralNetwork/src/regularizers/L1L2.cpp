8282a3df7c0d9648bbb6318d06a7734508eb9613
reapersy <reapersoy@proton.me>
2022-12-31 22:01:07 +0000

    remove testing code

--- NeuralNetwork/src/regularizers/L1L2.cpp
+++ NeuralNetwork/src/regularizers/L1L2.cpp
@@ -6,4 +6,7 @@ This file is part of neural-network
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be usefu
