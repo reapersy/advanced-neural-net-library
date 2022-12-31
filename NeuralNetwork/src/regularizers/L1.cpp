963d7d6e17944a93a071735a8870754adb23a87a
reapersy <reapersoy@proton.me>
2022-12-31 10:14:22 +0000

    prism

--- NeuralNetwork/src/regularizers/L1.cpp
+++ NeuralNetwork/src/regularizers/L1.cpp
@@ -26,4 +26,7 @@ namespace nn
 	{
 		void L1Regularizer::Regularize(const Matrix& weights, Matrix& gradient) const
 		{
-			gradient += Matrix::Map(weights, [l1 = m_L1](do
+			gradient += Matrix::Map(weights, [l1 = m_L1](double x) { return x >= 0 ? l1 : -l1; });
+		}
+	}
+}
