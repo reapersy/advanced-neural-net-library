a5cb263aa440704c080598b360764e89c414715e
reapersy <reapersoy@proton.me>
2022-12-31 19:08:46 +0000

    Add some docs.

--- NeuralNetwork/src/regularizers/L1L2.cpp
+++ NeuralNetwork/src/regularizers/L1L2.cpp
@@ -2,4 +2,8 @@
 Statically-linked deep learning library
 Copyright (C) 2020 Dušan Erdeljan, Nedeljko Vignjević
 
-This file is part o
+This file is part of neural-network
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the
