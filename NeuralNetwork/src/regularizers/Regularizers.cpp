adebaea827840805b1f57cb27e3a624336bf607c
reapersy <reapersoy@proton.me>
2023-01-05 07:55:30 +0000

    Minor code reformatting.

--- NeuralNetwork/src/regularizers/Regularizers.cpp
+++ NeuralNetwork/src/regularizers/Regularizers.cpp
@@ -1,2 +1,7 @@
 ﻿/*
-Statically-linked de
+Statically-linked deep learning library
+Copyright (C) 2020 Dušan Erdeljan, Nedeljko Vignjević
+
+This file is part of neural-network
+
+This program is free software: 
