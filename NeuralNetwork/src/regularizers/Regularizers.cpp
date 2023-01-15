8d078a0ae3c244199023a4f710e15655c54227fa
reapersy <reapersoy@proton.me>
2023-01-15 10:50:49 +0000

    Update file.py

--- NeuralNetwork/src/regularizers/Regularizers.cpp
+++ NeuralNetwork/src/regularizers/Regularizers.cpp
@@ -14,4 +14,12 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
+#include "Regularizers.h"
+
+namespace nn
+{
+	std::share
