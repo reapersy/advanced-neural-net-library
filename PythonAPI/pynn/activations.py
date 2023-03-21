84a475a7638e66321cd0195014dbab7c2fd4b8fa
reapersy <reapersoy@proton.me>
2023-03-21 22:04:55 +0000

    synced with kaleidoscope

--- PythonAPI/pynn/activations.py
+++ PythonAPI/pynn/activations.py
@@ -10,4 +10,6 @@ the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
-but WIT
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for mo
