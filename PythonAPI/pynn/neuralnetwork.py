82a52798b623a7526e96934657b34d6c2d13a1ea
reapersy <reapersoy@proton.me>
2023-04-08 22:58:17 +0000

    [maven-release-plugin] prepare release 0.4

--- PythonAPI/pynn/neuralnetwork.py
+++ PythonAPI/pynn/neuralnetwork.py
@@ -14,4 +14,10 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General 
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+from pynn.util.dll import DLLUtil
+from pynn.layers.dense import Dense
+from pynn.type.outp
