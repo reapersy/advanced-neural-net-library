5d8650aa9dce9c8c5817e52421796663978296dc
reapersy <reapersoy@proton.me>
2023-07-26 11:50:15 +0000

    Remove unused import

--- PythonAPI/pynn/state.py
+++ PythonAPI/pynn/state.py
@@ -14,4 +14,11 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy 
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+from pynn.optimizers import get_optimizer
+
+
+clas
