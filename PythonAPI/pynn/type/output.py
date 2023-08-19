d19ae9fa5ee1119021cbf1417e77b39dc1d399ed
reapersy <reapersoy@proton.me>
2023-08-19 19:44:51 +0000

    fml

--- PythonAPI/pynn/type/output.py
+++ PythonAPI/pynn/type/output.py
@@ -4,4 +4,6 @@ Copyright (C) 2020 Dušan Erdeljan, Nedeljko Vignjević
 
 This file is part of neural-network
 
-This pro
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either ver
