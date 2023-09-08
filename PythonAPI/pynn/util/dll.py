31f7684f05a728fa38dd34de03f79304a061c7fc
reapersy <reapersoy@proton.me>
2023-09-08 12:33:22 +0000

    version 1.3.3

--- PythonAPI/pynn/util/dll.py
+++ PythonAPI/pynn/util/dll.py
@@ -15,4 +15,13 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program.  If
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+from pynn.type.output import Output
+from pynn.layers.dense import Dense
+import ctypes as C
+import numpy as np
+import os
+
+
